62c78c084c2fdd7ee09e11987da7ae6ad08d0e2c
Danil Valiev <dnlvlv39@gmail.com>
2026-05-19 17:16:33 +0300

    feat: инициализатор бургер меню

--- scripts/core.js
+++ scripts/core.js
@@ -1,1 +1,23 @@
 // Общий для всех страниц скрипт
+
+export function initBurgerMenu() {
+    const burgerBtn = document.querySelector('.burger-menu-btn');
+    const navMenu = document.querySelector('.desktop-menu');
+
+    // Проверка существования элементов на странице
+    if (!burgerBtn || !navMenu) return;
+
+    // Открытие/закрытие по клику на бургер
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.toggle('is-active');
+        navMenu.classList.toggle('is-open');
+    });
+
+    // Закрытие меню при клике на любую ссылку внутри него
+    navMenu.addEventListener('click', (event) => {
+        if (event.target.classList.contains('top-menu-button')) {
+            burgerBtn.classList.remove('is-active');
+            navMenu.classList.remove('is-open');
+        }
+    });
+}
