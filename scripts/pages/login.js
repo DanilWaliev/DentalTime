27d657dcdd5a7acf69b152f49305ebbc3e254e37
Danil Valiev <dnlvlv39@gmail.com>
2026-05-20 04:14:15 +0300

    feat: добавлена обработка нажатия на кнопку вход на странице входа

--- scripts/pages/login.js
+++ scripts/pages/login.js
@@ -1,7 +1,29 @@
 import { initBurgerMenu } from '../core.js';
 
+let LoginState = {
+    username: null,
+    password: null,
+}
+
 function init() {
     initBurgerMenu();
+
+    const loginForm = document.querySelector('.login-form');
+    if (loginForm) {
+        loginForm.addEventListener('submit', handleSubmit);
+    }
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+    LoginState.username = usernameInput ? usernameInput.value : null;
+    LoginState.password = passwordInput ? passwordInput.value : null;
+
+    console.log("Данные для входа:", LoginState);
 }
 
 document.addEventListener('DOMContentLoaded', init);
