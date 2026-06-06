e6e4c82f2537aba5023ba07b492ad5d4a064564d
Danil Valiev <dnlvlv39@gmail.com>
2026-06-07 02:02:28 +0300

    feat: базовые методы сервисов

---
+++ internal/service/doctor.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"context"
+	"dental-time/internal/domain"
+)
+
+type DoctorRepository interface {
+	GetAll(ctx context.Context) ([]*domain.Doctor, error)
+	GetByID(ctx context.Context, id int) (*domain.Doctor, error)
+	GetBySpecialization(ctx context.Context, spec string) ([]*domain.Doctor, error)
+	Create(ctx context.Context, doctor domain.Doctor) (*domain.Doctor, error)
+	Update(ctx context.Context, doctor domain.Doctor) (*domain.Doctor, error)
+	Delete(ctx context.Context, id int) error
+}
+
+type DoctorService struct {
+	doctorRepo DoctorRepository
+}
+
+func (s *DoctorService) GetAll(ctx context.Context) ([]*domain.Doctor, error) {
+	return s.doctorRepo.GetAll(ctx)
+}
+
+func (s *DoctorService) GetByID(ctx context.Context, id int) (*domain.Doctor, error) {
+	return s.doctorRepo.GetByID(ctx, id)
+}
+
+func (s *DoctorService) GetBySpecialization(ctx context.Context, spec string) ([]*domain.Doctor, error) {
+	return s.doctorRepo.GetBySpecialization(ctx, spec)
+}
+
+func (s *DoctorService) Create(ctx context.Context, doctor domain.Doctor) (*domain.Doctor, error) {
+	return s.doctorRepo.Create(ctx, doctor)
+}
+
+func (s *DoctorService) Update(ctx context.Context, doctor domain.Doctor) (*domain.Doctor, error) {
+	return s.doctorRepo.Update(ctx, doctor)
+}
+
+func (s *DoctorService) Delete(ctx context.Context, id int) error {
+	return s.doctorRepo.Delete(ctx, id)
+}
