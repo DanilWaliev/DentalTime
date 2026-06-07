479afccd6d451362e67a8ecc4e800e7762cf8d66
Danil Valiev <dnlvlv39@gmail.com>
2026-06-07 17:25:19 +0300

    feat: кастомные ошибки и интерфейс для репозитория

---
+++ internal/service/service.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+	"dental-time/internal/domain"
+	"errors"
+)
+
+var ErrServiceNotFound = errors.New("service not found")
+var ErrServiceAlreadyExists = errors.New("service already exists")
+var ErrInvalidServiceID = errors.New("invalid service id")
+var ErrInvalidServiceData = errors.New("invalid service data")
+
+type ServicesRepository interface {
+	GetAll(ctx context.Context) ([]*domain.Service, error)
+	GetByID(ctx context.Context, id int) (*domain.Service, error)
+	GetByTitle(ctx context.Context, title string) (*domain.Service, error)
+	Create(ctx context.Context, service domain.Service) (*domain.Service, error)
+	Update(ctx context.Context, service domain.Service) (*domain.Service, error)
+	Delete(ctx context.Context, id int) error
+}
+
+type ServicesService struct {
+	servicesRepo ServicesRepository
+}
